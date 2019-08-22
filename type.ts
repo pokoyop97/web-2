c1269f80e4a788f0544025758fab958444864e80
pokoyo <zxdddf@gmail.com>
2019-08-21 19:48:15 -0600

    segundo ejercicio

---
+++ type.ts
@@ -0,0 +1,5 @@
+function area(forma: string, ancho: number, altura: number){
+    var area = ancho * altura;
+    return "yo soy un"+forma+" con un area de "+area+ " cm cuadrados.";
+}
+document.body.innerHTML = area("rectangulo",30,15);
