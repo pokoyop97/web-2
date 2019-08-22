9a42a4391162525d6b0e0bbcea4de09b8bf44cb9
pokoyo <zxdddf@gmail.com>
2019-08-22 14:01:20 -0600

    ejercicio 6 herencia

--- accesibilidad.js
+++ accesibilidad.js
@@ -1,8 +1,21 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Shape = /** @class */ (function () {
     function Shape(name, width, height) {
         this.name = name;
         this.area = width * height;
-        this.color = "un robot";
+        this.color = "rosa";
     }
     ;
     Shape.prototype.shoutout = function () {
@@ -10,10 +23,26 @@ var Shape = /** @class */ (function () {
     };
     return Shape;
 }());
+var Shape3D = /** @class */ (function (_super) {
+    __extends(Shape3D, _super);
+    function Shape3D(name, ancho, alto, largo) {
+        var _this = _super.call(this, name, ancho, alto) || this;
+        _this.name = name;
+        _this.volume = largo * _this.area;
+        return _this;
+    }
+    ;
+    Shape3D.prototype.shoutout = function () { return "yo soy " + this.name + " con un volumen de " + this.volume + " cm cubicos."; };
+    Shape3D.prototype.superShout = function () { return _super.prototype.shoutout.call(this); };
+    return Shape3D;
+}(Shape));
 var cuadrado = new Shape("cuadrado", 30, 30);
+var cube = new Shape3D("cube", 30, 30, 30);
 console.log(cuadrado.shoutout());
 console.log('Area de figura: ' + cuadrado.area);
 console.log('Name de figura: ' + cuadrado.name);
 console.log('Color de figura: ' + cuadrado.color);
 console.log('Ancho de figura: ' + cuadrado.width);
 console.log('Alto de figura: ' + cuadrado.height);
+console.log(cube.shoutout());
+console.log(cube.superShout());
--- accesibilidad.ts
+++ accesibilidad.ts
@@ -5,15 +5,26 @@ class Shape {
  
     constructor ( public name: string, width: number, height: number ) {
         this.area = width * height;
-        this.color = "un robot";
+        this.color = "rosa";
     };
  
     shoutout() {
         return "yo soy " + this.color + " " + this.name +  " con un area de " + this.area + " cm cuadrados.";
     }
 }
+class Shape3D extends Shape{
+    volume: number;
+    constructor (public name: string, ancho: number, alto: number, largo: number){
+        super(name,ancho,alto);
+        this.volume = largo *this.area;
+    };
+    shoutout() {        return "yo soy " + this.name +  " con un volumen de " + this.volume + " cm cubicos.";    }
+ 
+    superShout() {        return super.shoutout();    } 
+}
  
 var cuadrado = new Shape("cuadrado", 30, 30);
+var cube = new Shape3D("cube", 30, 30, 30); 
  
 console.log( cuadrado.shoutout() );
 console.log( 'Area de figura: ' + cuadrado.area );
@@ -21,3 +32,5 @@ console.log( 'Name de figura: ' + cuadrado.name );
 console.log( 'Color de figura: ' + cuadrado.color );
 console.log( 'Ancho de figura: ' + cuadrado.width );
 console.log( 'Alto de figura: ' + cuadrado.height );
+console.log( cube.shoutout() ); 
+console.log( cube.superShout() );
