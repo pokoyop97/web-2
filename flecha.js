002eb50e8afe6c7c7ceba689ed4378b91daaca1e
pokoyo <zxdddf@gmail.com>
2019-08-21 20:47:12 -0600

    ejercicio 4

---
+++ flecha.js
@@ -0,0 +1,12 @@
+var shape = {
+    name: "rectangulo",
+    popup: function () {
+        console.log('Esto es un popup():' + this.name);
+        setTimeout(function () {
+            console.log('Esto es dentro del Timeout():' + this.name);
+            console.log("yo soy " + this.name + "!");
+        }, 3000);
+    }
+};
+
+shape.popup();
