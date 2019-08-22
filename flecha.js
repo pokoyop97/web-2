42f56cbf693d47a066bf1ff5b56845b9790a6992
pokoyo <zxdddf@gmail.com>
2019-08-21 20:52:09 -0600

    ejercicio 4 segundo commit

--- flecha.js
+++ flecha.js
@@ -1,12 +1,12 @@
 var shape = {
     name: "rectangulo",
     popup: function () {
+        var _this = this;
         console.log('Esto es un popup():' + this.name);
         setTimeout(function () {
-            console.log('Esto es dentro del Timeout():' + this.name);
-            console.log("yo soy " + this.name + "!");
+            console.log('Esto es dentro del Timeout():' + _this.name);
+            console.log("yo soy " + _this.name + "!");
         }, 3000);
     }
 };
-
 shape.popup();
--- flecha.ts
+++ flecha.ts
@@ -2,9 +2,10 @@ var shape = {
     name: "rectangulo",
     popup: function(){
         console.log('Esto es un popup():'+this.name);
-        setTimeout(function(){
+        setTimeout(()=>{
             console.log('Esto es dentro del Timeout():'+this.name);
             console.log("yo soy "+this.name+"!");
         },3000);
     }
 };
+shape.popup();
