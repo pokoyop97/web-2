799233ccfc48889e866227e0ad57658a7d8466c3
pokoyo <zxdddf@gmail.com>
2019-08-26 20:32:16 -0600

    tipot segundo commit

--- tipot.js
+++ tipot.js
@@ -1,1 +1,8 @@
-function removeChar(theString, theChar) { var theRegex = new RegExp(theChar, "gi"); return theString.replace(theRegex, ''); }
+function removeChar(theString, theChar) {
+    var theRegex = new RegExp(theChar, "gi");
+    return theString.replace(theRegex, '');
+}
+function removeIt(theInput, theIt) {
+    var theRegex = new RegExp(theIt, "gi");
+    return theInput.replace(theRegex, '');
+}
--- tipot.ts
+++ tipot.ts
@@ -2,3 +2,7 @@ function removeChar(theString: string, theChar: string): string {
     let theRegex = new RegExp(theChar, "gi");    
     return theString.replace(theRegex, ''); 
 }
+function removeIt<T>(theInput: T, theIt: string): T {    
+    let theRegex = new RegExp(theIt, "gi");    
+    return theInput.replace(theRegex, ''); 
+}
